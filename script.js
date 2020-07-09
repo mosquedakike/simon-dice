26927cdf495ac2727c7a5054fd764494828db1d0
Enrique <enrique_inf@hotmail.com>
2020-07-08 22:34:44 -0500

    adding color sequence

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const violeta = document.getElementById('violeta')
 const naranja = document.getElementById('naranja')
 const verde = document.getElementById('verde')
 
+debugger
 class Juego{
     constructor(){
       this.inicializar()
       this.generarSecuencia()
+      this.siguienteNivel()
       }
 
       inicializar(){
         btnEmpezar.classList.add('hide')
         this.nivel = 1
-        this.colores= {
+        this.colores = {
             celeste,
             violeta,
             naranja,
@@ -22,7 +24,46 @@ class Juego{
       }
 
       generarSecuencia(){
-        this.generarSecuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+      }
+
+      siguienteNivel(){
+          this.iluminarSecuencia()
+      }
+
+      transformarNumeroAColor(numero){
+        switch (numero) {
+            case 0:
+                return 'celeste'
+            case 1:
+                return 'violeta'
+            case 2:
+                return 'naranja'    
+            case 3:
+                return 'verde'
+        }
+      }
+
+      iluminarSecuencia(){
+          for(let i = 0; i < this.nivel; i++){
+            const color = this.transformarNumeroAColor(this.secuencia[i])
+            console.log(color)
+            
+            setTimeout(() => {
+              console.log(color)
+            this.iluminarColor(color)
+            }, 1000 * i)
+              
+          }
+      }
+
+      iluminarColor(color){
+        this.colores[color].classList.add('light')
+        setTimeout(() => this.apagarColor(color),350)
+      }
+
+      apagarColor(color){
+        this.colores[color].classList.remove('light')
       }
     }
   
