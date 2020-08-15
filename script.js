4377bd405e20993eadf833533cca583eed4a14f2
Enrique <enrique_inf@hotmail.com>
2020-08-14 22:21:12 -0500

    Generando una secuencia de numeros

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//definimos constantes para obetener los ID's de las etiquetas HTML
 const btnEmpezar = document.getElementById('btnEmpezar')
 const celeste = document.getElementById('celeste')
 const violeta = document.getElementById('violeta')
@@ -11,6 +12,8 @@ class Juego{
       }
 
       inicializar(){
+        //ocultamos el boton de empezar al dar click con la 
+        //propiedad display:none definida en el .css
         btnEmpezar.classList.add('hide')
         this.nivel = 1
         this.colores= {
@@ -18,15 +21,24 @@ class Juego{
             violeta,
             naranja,
             verde
-        }
+        } 
       }
 
       generarSecuencia(){
-        this.generarSecuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4))
+        //generamos un array de 10 posicones
+        //usamos el metodo fill para inciializar en 0 las diez posiciones
+        //metodo map
+        //multiplicamos *4 para que devuelva numeros del 1 al 3
+       
+        //this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
+        this.secuencia = new Array(10).fill(0).map(function n(){
+          return Math.floor(Math.random()*4)
+          }) 
+        }
       }
-    }
   
 
   function empezarJuego() {
+    debugger
      window.juego = new Juego()
   }
